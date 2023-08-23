04f623df759cfe05db0b35eb6fd8ad73cf116aa0
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-08-23 19:57:59 +0600

    single category page created

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "./page.module.css";
 
-const Category = () => {
-  return <div>Category</div>;
+const Category = ({ params }) => {
+  return (
+    <div className={styles.category}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+    </div>
+  );
 };
 
 export default Category;
