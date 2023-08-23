7954ba8e64383ee5b3bbf92a4dffbc16018493f0
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-08-23 15:52:04 +0600

    react spinner added in loading state of contact

--- src/app/contact/loading.jsx
+++ src/app/contact/loading.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { ClimbingBoxLoader } from "react-spinners";
+import styles from "./loading.module.css";
 
 const loading = () => {
-  return <div>loading</div>;
+  return (
+    <div className={styles.container}>
+      <ClimbingBoxLoader color="#194c33" />
+    </div>
+  );
 };
 
 export default loading;
