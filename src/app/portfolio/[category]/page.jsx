cb9621398baa32b00d284e42dfc42363e321c8dd
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-08-23 20:09:34 +0600

    portfolio details page added

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,10 +1,38 @@
 import React from "react";
 import styles from "./page.module.css";
+import Button from "src/components/Button/Button";
+import Image from "next/image";
 
 const Category = ({ params }) => {
   return (
     <div className={styles.category}>
       <h1 className={styles.catTitle}>{params.category}</h1>
+
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>sdasda</h1>
+          <p className={styles.desc}>abd</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src="/illustration.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>sdasda</h1>
+          <p className={styles.desc}>abd</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image className={styles.img} fill={true} src="/apps.jpg" alt="" />
+        </div>
+      </div>
     </div>
   );
 };
