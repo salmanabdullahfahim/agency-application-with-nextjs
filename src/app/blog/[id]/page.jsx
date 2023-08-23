4f61dbba71e120ca4c010eedf62c901f7b1060c5
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-08-23 20:24:51 +0600

    single blog details page added

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
 
-const BlogPost = () => {
-  return <div>BlogPost</div>;
+const BlogPost = ({ params }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>adfas</h1>
+          <p className={styles.desc}>fef</p>
+          <div className={styles.author}>
+            <Image
+              src="/illustration.png"
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>dasdas</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image src="/apps.jpg" alt="" fill={true} className={styles.image} />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>dasds</p>
+      </div>
+    </div>
+  );
 };
 
 export default BlogPost;
