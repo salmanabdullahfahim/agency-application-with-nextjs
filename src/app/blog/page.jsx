19b6f38afdc787e32a1cb362747e68ec9807f148
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-08-23 20:16:45 +0600

    blog page added

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const Blog = () => {
-  return <div className={styles.container}>Blog</div>;
+  return (
+    <div className={styles.mainContainer}>
+      <Link href="/blog/test" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="/illustration.png"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Title</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Blog;
