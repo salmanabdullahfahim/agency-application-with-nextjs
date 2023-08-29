3301886b54a0b27f5dfceead0e4c5780008988fc
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-08-29 17:27:21 +0600

    postSchema added

--- models/Post.js
+++ models/Post.js
@@ -1,3 +1,33 @@
 import mongoose from "mongoose";
 
 const { Schema } = mongoose;
+
+const postSchema = new Schema(
+  {
+    title: {
+      type: "string",
+
+      required: true,
+    },
+    desc: {
+      type: "string",
+
+      required: true,
+    },
+    img: {
+      type: "string",
+      required: true,
+    },
+    content: {
+      type: "string",
+      required: true,
+    },
+    username: {
+      type: "string",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Post", postSchema);
