bc6d3d06f78de6b5ac062ab866593b66c0025dfb
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-08-29 15:59:59 +0600

    mongoose setup done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,9 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.item}>
-        <h1 className={styles.title}>Better Design for yor digital products</h1>
+        <h1 className={styles.title}>
+          Better Design for your digital products
+        </h1>
         <p className={styles.desc}>
           Turning your Idea into Reality. We bring together the teams from the
           global tech industry
