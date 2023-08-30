20ac111f80d80d7540979b1b33029255d1f2bdbd
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-08-30 15:59:57 +0600

    private route added in the dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,10 +3,15 @@
 import { useSession } from "next-auth/react";
 import styles from "./page.module.css";
 import useSWR from "swr";
+import { useRouter } from "next/navigation";
 
 const Dashboard = () => {
   const session = useSession();
   console.log(session);
+
+  const router = useRouter();
+
+  //fetch
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
   const { data, error, isLoading } = useSWR(
@@ -14,7 +19,16 @@ const Dashboard = () => {
     fetcher
   );
 
-  return <div className={styles.container}>Dashboard</div>;
+  if (session.status === "loading") {
+    return <p>Loading ....</p>;
+  }
+  if (session.status === "unauthenticated") {
+    router?.push("/dashboard/login");
+  }
+
+  if (session.status === "authenticated") {
+    return <div className={styles.container}>Dashboard</div>;
+  }
 };
 
 export default Dashboard;
