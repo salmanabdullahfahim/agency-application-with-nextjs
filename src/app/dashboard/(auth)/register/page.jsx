b543b14641a5f25000b434b3ea8c4419bd58ce43
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-08-30 13:18:29 +0600

    register page ui added

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Create an Account</h1>
+      <h2 className={styles.subtitle}>Please sign up to see the dashboard.</h2>
+      <form className={styles.form}>
+        <input
+          type="text"
+          placeholder="Username"
+          required
+          className={styles.input}
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          required
+          className={styles.input}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          className={styles.input}
+        />
+        <button className={styles.button}>Register</button>
+      </form>
+      <span className={styles.or}>- OR -</span>
+      <Link className={styles.link} href="/dashboard/login">
+        Login with an existing account
+      </Link>
+    </div>
+  );
 };
 
 export default Register;
