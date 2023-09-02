d2f6c6b0708808603fd8f8c61553257a87528d24
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-09-02 15:50:53 +0600

    vercel backend api link added

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -4,9 +4,12 @@ import Image from "next/image";
 import { notFound } from "next/navigation";
 
 async function getData(id) {
-  const res = await fetch(``, {
-    cache: "no-store",
-  });
+  const res = await fetch(
+    `https://agency-application-with-nextjs-2xkrzj85n-salmanabdullahfahim.vercel.app/api/posts/${id}`,
+    {
+      cache: "no-store",
+    }
+  );
 
   if (!res.ok) {
     return notFound();
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,9 +4,12 @@ import Link from "next/link";
 import Image from "next/image";
 
 async function getData() {
-  const res = await fetch("", {
-    cache: "no-store",
-  });
+  const res = await fetch(
+    "https://agency-application-with-nextjs-2xkrzj85n-salmanabdullahfahim.vercel.app/api/posts",
+    {
+      cache: "no-store",
+    }
+  );
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
